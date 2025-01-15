97d640ba00a196d9d10a446e5a7fa5f029c90630
Lililatortue <will.descoteaux@gmail.com>
2025-01-15 00:03:27 -0500

    mis les variables index du directory public

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php 
+//variable
+enum Categorie
+{//nomenclature-> majuscule snakecase
+    case VINS_NON_ALCOOLISES;
+    case PRODUIT_POUR_BEBE;
+    case JOUETS;
+    case VOITURES_ELECTRIQUES_NEUVES;
+    case AUTRE;
+}
+//nomenclature variable-> snakecase minuscule
+$user_name
+$produit
+//minimum 1 je l'initialise immediatement
+$quantite=1;
+$date_livraison;
+
+
+?>
